1d3610810116a435dd3bad7f3aced2f7d121d363
Sufyan Khan <khan.sufyan.cs@gmail.com>
2023-04-24 19:34:53 +0200

    feat: set up clerk and tailwind UI deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,22 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        <title>JotIt</title>
+        <meta name="description" content="Efficient Note-Taking" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </Head>
+
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
