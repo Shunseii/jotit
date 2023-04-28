987b8adb56a10f337175dc3ba83524634b32b9ba
Sufyan Khan <khan.sufyan.cs@gmail.com>
2023-04-29 01:31:32 +0200

    chore: setup some packages and tailwind utils

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ export default {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      gridTemplateColumns: {
+        "fill-xs": "repeat(auto-fill, minmax(20rem, 1fr))",
+        "fill-sm": "repeat(auto-fill, minmax(24rem, 1fr))",
+        "fill-md": "repeat(auto-fill, minmax(28rem, 1fr))",
+        "fill-lg": "repeat(auto-fill, minmax(32rem, 1fr))",
+        "fill-xl": "repeat(auto-fill, minmax(36rem, 1fr))",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
