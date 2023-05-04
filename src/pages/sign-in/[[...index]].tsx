2721ae6f4c8225ca0c85058665b68fe718c997b5
Sufyan Khan <khan.sufyan.cs@gmail.com>
2023-05-04 21:11:51 +0200

    feat: add dark mode to sign in and sign up pages

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -1,13 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
-const SignInPage = () => (
-  <SignIn
-    path="/sign-in"
-    routing="path"
-    signUpUrl="/sign-up"
-    afterSignInUrl="/"
-    appearance={{ elements: { rootBox: "mx-auto" } }}
-  />
-);
+const SignInPage = () => {
+  const { theme } = useTheme();
+
+  return (
+    <SignIn
+      path="/sign-in"
+      routing="path"
+      signUpUrl="/sign-up"
+      afterSignInUrl="/"
+      appearance={{
+        elements: { rootBox: "mx-auto" },
+        baseTheme: theme === "dark" ? dark : undefined,
+      }}
+    />
+  );
+};
 
 export default SignInPage;
--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,13 +1,20 @@
 import { SignUp } from "@clerk/nextjs";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
 
-const SignUpPage = () => (
+const SignUpPage = () => {
+  const { theme } = useTheme();
+
   <SignUp
     path="/sign-up"
     routing="path"
     signInUrl="/sign-in"
     afterSignUpUrl="/"
-    appearance={{ elements: { rootBox: "mx-auto" } }}
-  />
-);
+    appearance={{
+      elements: { rootBox: "mx-auto" },
+      baseTheme: theme === "dark" ? dark : undefined,
+    }}
+  />;
+};
 
 export default SignUpPage;
